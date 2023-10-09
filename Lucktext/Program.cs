c88cbbd379648d246758e39fa7c1fda81fe3eac5
hugostenarson <hugo.stenarson@elev.ga.ntig.se>
2023-10-09 08:28:46 +0200

    hugo

---
+++ Lucktext/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vad är ditt namn?");
+
+string namn = Console.ReadLine();
+
+Console.WriteLine("Hej " + namn + ", vad är din favoritmaträtt?");
+
+string favoritmaträtt = Console.ReadLine();
+Console.WriteLine("Mums, " + favoritmaträtt + " är gott!");
+Console.WriteLine("Vilken veckodag är det idag?");
+string veckodag = Console.ReadLine();
+Console.WriteLine("Ha en bra " + veckodag + " " + namn + " och kämpa hårt!");
+
+Console.ReadLine();
